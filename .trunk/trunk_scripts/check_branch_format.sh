547e0030065fa28fa84b30d31139452795ae3bdb
Eduardo Nuez Wehbe <95353696+EN2U@users.noreply.github.com>
2024-02-07 22:22:14 +0000

    chore: add trunk-io tool, configure branch and commit conventions (#1)
    * chore: Add trunk package and configure git branches and commits formats
    
    * chore: add commitlint instead of checking manually commit messages
    
    * chore: add trunk into devbox initial script and check branch format in pre-commit hook

---
+++ .trunk/trunk_scripts/check_branch_format.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+ORANGE="\033[0;33m"
+NOCOLOR="\033[0m"
+
+branch_name=$(git symbolic-ref --short HEAD)
+
+if [ "$branch_name" == "main" ]; then
+    exit 0
+fi
+
+allowed_pattern="^(feature|chore|fix|docs)/.*$"
+
+if [[ ! $branch_name =~ $allowed_pattern ]]; then
+    echo -e "\n${RED}Error: Branch naming must starts with 'chore/', 'feature/', 'docs/' or 'fix'/"
+    exit 1
+fi
+
+exit 0
