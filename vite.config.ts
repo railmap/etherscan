007236a28d2c585e31fe6b45efaa9f61f34de17d
Óscar Carrasco <oxcabe@gmail.com>
2024-02-09 18:05:03 +0000

    chore: add vitest, configure eslint and vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import eslint from "vite-plugin-eslint";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  plugins: [eslint(), tsconfigPaths()],
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'etherscan',
-      fileName: 'etherscan',
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "etherscan",
+      fileName: "etherscan",
     },
   },
-})
+  test: {
+    includeSource: ["src/**/*.{ts}"],
+    include: ["test/**/*.{test,spec}.ts?(x)"],
+  },
+});
